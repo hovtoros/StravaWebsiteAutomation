bf14b454036eadc8aca3be6dff1697d8f1d5fe93
Hovhannes Torosyan <i-HovhannesT@dom-daniel.com>
2020-05-07 13:36:51 +0400

    Implemented recordFailure public method in BaseTests.java.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,18 +1,27 @@
 package base;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import utils.EventReporter;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BaseTests {
     private String websiteLink = "https://www.strava.com/";
@@ -40,6 +49,23 @@ public class BaseTests {
     @AfterClass
     public void tearDown() {
         firefoxDriver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus())
+        {
+            var camera = (TakesScreenshot)firefoxDriver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try
+            {
+                Files.move(screenshot.toPath(), new File(Paths.get("resources", "screenshots", result.getName() + ".png").toString()).toPath());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     protected void FillLoginForm(String email, String password){
