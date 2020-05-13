d0d6038c8b810b4049e52e4edf4384cc4176011e
Hovhannes Torosyan <i-HovhannesT@dom-daniel.com>
2020-05-13 23:45:47 +0400

    Corrected some constant values for the tests.

--- src/test/java/constants/LoginFormConstants.java
+++ src/test/java/constants/LoginFormConstants.java
@@ -1,9 +1,9 @@
 package constants;
 
 public class LoginFormConstants {
-    public static final String ValidEmail = "8dasaran@gmail.com";
+    public static final String ValidEmail = "hovhannes_torosyan@edu.aua.am";
     public static final String InvalidEmail = "invalid";
-    public static final String ValidPassword = "bigwin13";
+    public static final String ValidPassword = "dummypass1234";
     public static final String InvalidPassword = "invalid";
     public static final String FormLabel = "Log In";
     public static final String ErrorMessage = "The username or password did not match. Please try again.";
